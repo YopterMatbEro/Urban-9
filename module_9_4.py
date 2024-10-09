2fcd5b3982d3b9698c4c126904484220e717d871
YopterMatbEro <yopter2013@gmail.com>
2024-10-09 09:12:15 +0300

    functional diversity

--- module_9_4.py
+++ module_9_4.py
@@ -11,7 +11,7 @@ class MysticBall:
 
 def get_advanced_writer(file_name):
     def write_everything(*data_set):
-        with open(file_name, '+a', encoding='utf-8') as f:
+        with open(file_name, 'w', encoding='utf-8') as f:
             for data in data_set:
                 f.write(f'{str(data)}\n')
 
